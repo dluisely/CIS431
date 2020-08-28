ead6791b1e8481c04eee9fc53a9102459a566953
Luisely Doza <dluisely10999@gmail.com>
2020-08-28 18:28:37 -0400

    Creating Closest Pair

---
+++ ClosestPair.java
@@ -0,0 +1,16 @@
+import edu.princeton.cs.introcs.*;
+import edu.princeton.cs.algs4.*;
+/**
+ * 1.2.1 Write a Point2D client that takes an integer value N from the command line, generates N random points in the unit square, and computes the distance separating the closest pair of points.
+ */
+public class ClosestPair {
+    public static void showPoints() {
+        
+        double [] points = {0.40,0.61,0.85,0.57,0.22};
+        edu.princeton.cs.algs4.StdStats.plotPoints(points);
+    }
+    
+    public static void main(String args[]) {
+        
+    }
+}
